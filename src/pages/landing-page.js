1e7e23fc4aa9c05e877771f82d321f95acdbe72d
Martha Huaman <ms.huamann@gmail.com>
2023-01-11 19:23:25 -0500

    ADD: search section in landing page (made by Anthony & Martha)

--- src/pages/landing-page.js
+++ src/pages/landing-page.js
@@ -9,6 +9,14 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
+const SearchSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 66px;
+`;
+
 const StyledForm = styled("form")`
   display: flex;
   flex-direction: row;
@@ -67,84 +75,90 @@ function LandingPage() {
     <Wrapper>
       <div>AQUI VA EL Landing PAGE</div>
       {/* <LandingBackground/> */}
-      <p>Meet your new Home</p>
-      <p>The easiest way to find where you belong</p>
+      <SearchSection>
+        <Wrapper>
+          <p style={{ fontFamily: "Montserrat", fontWeight: "300", fontSize: "64px",
+            lineHeight: "88px", letterSpacing: "-0.5px", color: "#373737"}}>Meet your new Home</p>
+          <p style={{ fontFamily: "Montserrat", fontWeight: "400", fontSize: "24px",
+            lineHeight: "32px", color: "#616161"}}>The easiest way to find where you belong</p>
+        </Wrapper>
 
-      <StyledForm >
+        <StyledForm >
+          <SelectBox>
+            <StyledSelectTitle>I'M LOOKING FOR</StyledSelectTitle>
+            <select required name="relation"  style={{ border: "none", height: "24px", width: "160px", fontFamily: "Inter", fontSize: "16px",
+                lineHeight: "24px", letterSpacing: "0.5px", color: "#373737"}}>
+              <option value="Apartment">An apartment</option>
+              <option value="House">A house</option>
+            </select>
+          </SelectBox>
 
-        <SelectBox>
-          <StyledSelectTitle>I'M LOOKING FOR</StyledSelectTitle>
-          <select required name="relation"  style={{ border: "none", height: "24px", width: "160px", fontFamily: "Inter", fontSize: "16px",
-              lineHeight: "24px", letterSpacing: "0.5px", color: "#373737"}}>
-            <option value="Apartment">An apartment</option>
-            <option value="House">A house</option>
-          </select>
-        </SelectBox>
+          <SelectBox>
+            <StyledSelectTitle>I WANT TO</StyledSelectTitle>
+            <select required name="relation"  style={{ border: "none", height: "24px", width: "160px", fontFamily: "Inter", fontSize: "16px",
+                lineHeight: "24px", letterSpacing: "0.5px", color: "#373737"}}>
+              <option value="Rent">Rent</option>
+              <option value="Sale">Sale</option>
+            </select>
+          </SelectBox>
 
-        <SelectBox>
-          <StyledSelectTitle>I WANT TO</StyledSelectTitle>
-          <select required name="relation"  style={{ border: "none", height: "24px", width: "160px", fontFamily: "Inter", fontSize: "16px",
-              lineHeight: "24px", letterSpacing: "0.5px", color: "#373737"}}>
-            <option value="Rent">Rent</option>
-            <option value="Sale">Sale</option>
-          </select>
-        </SelectBox>
+          <SelectBox>
+            <StyledSelectTitle>WHERE</StyledSelectTitle>
+            <select required name="relation"  style={{ border: "none", height: "24px", width: "304px", fontFamily: "Inter", fontSize: "16px",
+                lineHeight: "24px", letterSpacing: "0.5px", color: "#373737"}}>
+              <option disabled selected hidden>Favorite district</option>
+              <option value="Family">Ancón</option>
+              <option value="Family">Ate</option>
+              <option value="Friends">Barranco</option>
+              <option value="Work">Breña</option>
+              <option value="Acquaintance">Carabayllo</option>
+              <option value="Acquaintance">Cercado de Lima</option>
+              <option value="Acquaintance">Chorrillos</option>
+              <option value="Family">Cieneguilla</option>
+              <option value="Friends">Comas</option>
+              <option value="Work">El agustino</option>
+              <option value="Acquaintance">Independencia</option>
+              <option value="Acquaintance">La molina</option>
+              <option value="Acquaintance">La victoria</option>
+              <option value="Family">Lince</option>
+              <option value="Friends">Los olivos</option>
+              <option value="Work">Lurigancho</option>
+              <option value="Acquaintance">Lurín</option>
+              <option value="Acquaintance">Magdalena del mar</option>
+              <option value="Acquaintance">Miraflores</option>
+              <option value="Family">Pachacámac</option>
+              <option value="Family">Pucusana</option>
+              <option value="Friends">Pueblo libre</option>
+              <option value="Work">Puente piedra</option>
+              <option value="Acquaintance">Punta hermosa</option>
+              <option value="Acquaintance">Punta negra</option>
+              <option value="Acquaintance">Rímac</option>
+              <option value="Family">San bartolo</option>
+              <option value="Friends">San borja</option>
+              <option value="Work">San isidro</option>
+              <option value="Family">San Juan de Lurigancho</option>
+              <option value="Family">San Juan de Miraflores</option>
+              <option value="Friends">San Luis</option>
+              <option value="Work">San Martin de Porres</option>
+              <option value="Acquaintance">San Miguel</option>
+              <option value="Acquaintance">Santa Anita</option>
+              <option value="Acquaintance">Santa María del Mar</option>
+              <option value="Family">Santa Rosa</option>
+              <option value="Friends">Santiago de Surco</option>
+              <option value="Work">Surquillo</option>
+              <option value="Acquaintance">Villa el Salvador</option>
+              <option value="Acquaintance">Villa Maria del Triunfo</option>
+            </select>
+          </SelectBox>
 
-        <SelectBox>
-          <StyledSelectTitle>WHERE</StyledSelectTitle>
-          <select required name="relation"  style={{ border: "none", height: "24px", width: "304px", fontFamily: "Inter", fontSize: "16px",
-              lineHeight: "24px", letterSpacing: "0.5px", color: "#373737"}}>
-            <option disabled selected hidden>Favorite district</option>
-            <option value="Family">Ancón</option>
-            <option value="Family">Ate</option>
-            <option value="Friends">Barranco</option>
-            <option value="Work">Breña</option>
-            <option value="Acquaintance">Carabayllo</option>
-            <option value="Acquaintance">Cercado de Lima</option>
-            <option value="Acquaintance">Chorrillos</option>
-            <option value="Family">Cieneguilla</option>
-            <option value="Friends">Comas</option>
-            <option value="Work">El agustino</option>
-            <option value="Acquaintance">Independencia</option>
-            <option value="Acquaintance">La molina</option>
-            <option value="Acquaintance">La victoria</option>
-            <option value="Family">Lince</option>
-            <option value="Friends">Los olivos</option>
-            <option value="Work">Lurigancho</option>
-            <option value="Acquaintance">Lurín</option>
-            <option value="Acquaintance">Magdalena del mar</option>
-            <option value="Acquaintance">Miraflores</option>
-            <option value="Family">Pachacámac</option>
-            <option value="Family">Pucusana</option>
-            <option value="Friends">Pueblo libre</option>
-            <option value="Work">Puente piedra</option>
-            <option value="Acquaintance">Punta hermosa</option>
-            <option value="Acquaintance">Punta negra</option>
-            <option value="Acquaintance">Rímac</option>
-            <option value="Family">San bartolo</option>
-            <option value="Friends">San borja</option>
-            <option value="Work">San isidro</option>
-            <option value="Family">San Juan de Lurigancho</option>
-            <option value="Family">San Juan de Miraflores</option>
-            <option value="Friends">San Luis</option>
-            <option value="Work">San Martin de Porres</option>
-            <option value="Acquaintance">San Miguel</option>
-            <option value="Acquaintance">Santa Anita</option>
-            <option value="Acquaintance">Santa María del Mar</option>
-            <option value="Family">Santa Rosa</option>
-            <option value="Friends">Santiago de Surco</option>
-            <option value="Work">Surquillo</option>
-            <option value="Acquaintance">Villa el Salvador</option>
-            <option value="Acquaintance">Villa Maria del Triunfo</option>
-          </select>
-        </SelectBox>
+          <ButtonBox>
+            <StyledButton type="submit">
+              <ButtonText> Search </ButtonText>
+            </StyledButton>
+          </ButtonBox>
+        </StyledForm>
+      </SearchSection>
 
-        <ButtonBox>
-          <StyledButton type="submit">
-            <ButtonText> Search </ButtonText>
-          </StyledButton>
-        </ButtonBox>
-      </StyledForm>
 
       {/* <TeamMembers/> */}
     </Wrapper>
