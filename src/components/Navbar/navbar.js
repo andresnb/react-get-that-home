f9b3085088713f3489b22b07449d9f37bb8aae30
crhistoff <cechevarriamesias@gmail.com>
2023-01-18 23:46:28 -0500

    Feat: Add Login and Login-css

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,7 +1,7 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 // import PropertyCard from '../PropertyCard';
 import getThatHomeLogo from "../../assets/images/get-that-home-logo.png";
-import {AiOutlineUserAdd} from "react-icons/ai";
+import { AiOutlineUserAdd } from "react-icons/ai";
 import { FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
@@ -18,7 +18,7 @@ const NavBarContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   width: 1136px;
-  margin:16px 0px;
+  margin: 16px 0px;
 `;
 
 const NavBarItems = styled.div`
@@ -32,8 +32,8 @@ const NavBarItems = styled.div`
 const WhiteButton = styled("button")`
   height: 40px;
   border: none;
-  background: #FFFFFF;
-  border: 2px solid #F48FB1;
+  background: #ffffff;
+  border: 2px solid #f48fb1;
   border-radius: 16px;
   padding: 0px 8px;
 `;
@@ -41,48 +41,108 @@ const WhiteButton = styled("button")`
 const PinkButton = styled("button")`
   height: 40px;
   border: none;
-  background: #F48FB1;
+  background: #f48fb1;
   border-radius: 16px;
   padding: 0px 8px;
 `;
 
-function Navbar(){
+function Navbar() {
   return (
     <Wrapper>
       <NavBarContainer>
-        <Link  to={"/landing"}>
+        <Link to={"/landing"}>
           <img
-          src={getThatHomeLogo}
-          alt="get that home app logo"
-          style={{ height: "40px", width: "136px"}}
+            src={getThatHomeLogo}
+            alt="get that home app logo"
+            style={{ height: "40px", width: "136px" }}
           />
         </Link>
         <NavBarItems>
-          <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "6px"}}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "6px",
+            }}
+          >
             <FiSearch style={{ color: "#616161" }} />
-            <Link  to={"/properties"}>
-              <p style={{ fontFamily: "Inter", fontWeight: "500", fontSize: "14px",
-                lineHeight: "24px", letterSpacing: "1.25px", color: "#616161"}}>FIND A HOME</p>
+            <Link to={"/properties"}>
+              <p
+                style={{
+                  fontFamily: "Inter",
+                  fontWeight: "500",
+                  fontSize: "14px",
+                  lineHeight: "24px",
+                  letterSpacing: "1.25px",
+                  color: "#616161",
+                }}
+              >
+                FIND A HOME
+              </p>
             </Link>
           </div>
           <WhiteButton>
-            <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "4px"}}>
-              <AiOutlineUserAdd style={{width: "19px", height: "21px", color: "#616161"}}/>
-              <p style={{ fontFamily: "Inter", fontWeight: "500", fontSize: "14px",
-              lineHeight: "24px", letterSpacing: "1.25px", color: "#616161"}}>JOIN</p>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                gap: "4px",
+              }}
+            >
+              <AiOutlineUserAdd
+                style={{ width: "19px", height: "21px", color: "#616161" }}
+              />
+              <p
+                style={{
+                  fontFamily: "Inter",
+                  fontWeight: "500",
+                  fontSize: "14px",
+                  lineHeight: "24px",
+                  letterSpacing: "1.25px",
+                  color: "#616161",
+                }}
+              >
+                JOIN
+              </p>
             </div>
           </WhiteButton>
-          <PinkButton>
-            <div style={{display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center", gap: "4px"}}>
-              <AiOutlineUserAdd style={{width: "19px", height: "21px", color: "#FFFFFF"}}/>
-              <p style={{ fontFamily: "Inter", fontWeight: "500", fontSize: "14px",
-              lineHeight: "24px", letterSpacing: "1.25px", color: "#FFFFFF"}}>LOGIN</p>
-            </div>
-          </PinkButton>
+          <Link to="/login">
+            <PinkButton>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  gap: "4px",
+                }}
+              >
+                <AiOutlineUserAdd
+                  style={{ width: "19px", height: "21px", color: "#FFFFFF" }}
+                />
+                <p
+                  style={{
+                    fontFamily: "Inter",
+                    fontWeight: "500",
+                    fontSize: "14px",
+                    lineHeight: "24px",
+                    letterSpacing: "1.25px",
+                    color: "#FFFFFF",
+                  }}
+                >
+                  LOGIN
+                </p>
+              </div>
+            </PinkButton>
+          </Link>
         </NavBarItems>
       </NavBarContainer>
     </Wrapper>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
